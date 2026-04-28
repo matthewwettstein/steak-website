c3f0a3ea987c06354025329df5c8573913df2057
matthewwettstein <matthew.wettstein@mcvts.org>
2026-04-28 09:07:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+function handleSubmit(event) {
+    event.preventDefault();
+   
+    let name = document.getElementById("name").value;
+    let message = document.getElementById("message").value;
+   
+    if (name === "" || message === "") {
+      document.getElementById("response").innerText =
+        "Please fill out all fields.";
+    } else {
+      document.getElementById("response").innerText =
+        "Thank you, " + name + ". We will respond soon.";
+    }
+   }
+   
+   
+   
